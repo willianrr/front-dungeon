937b4bef96871c1f483b43c199037ee9df800965
willianrr <willianrogerioregina@gmail.com>
2026-06-29 18:51:55 -0300

    allow host dinamic

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,10 @@ export default defineConfig({
   server: {
     port: 5173,
     open: true,
-    allowedHosts: ['.loca.lt', '.trycloudflare.com'],
+    allowedHosts: ['.loca.lt', '.trycloudflare.com', 'front-dungeon-764196a4.mithrill.com.br'],
   },
   preview: {
-    allowedHosts: ['.loca.lt', '.trycloudflare.com'],
+    allowedHosts: ['.loca.lt', '.trycloudflare.com', 'front-dungeon-764196a4.mithrill.com.br'],
   },
   build: {
     target: 'es2022',
